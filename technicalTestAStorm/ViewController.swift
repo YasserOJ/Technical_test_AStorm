b8e243e48e3edefec971c3e4e5d4371d9e244b7f
Yasser Omar Jammeli <yasser-omar.jammeli@proxym-it.com>
2022-08-22 20:31:30 +0100

    add project default files

---
+++ technicalTestAStorm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  technicalTestAStorm
+//
+//  Created by Yasser Omar Jammeli on 22/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
