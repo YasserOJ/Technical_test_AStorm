708a68e1317e7b0c9848107ba2f66aa0091abf4a
Yasser Omar Jammeli <yasser.omar.jammeli@gmail.com>
2022-08-24 23:50:51 +0100

    finish weather overview screen dev

--- technicalTestAStorm/Modules/weather/Weather Overview/View Model/WeatherOverViewViewModel.swift
+++ technicalTestAStorm/Modules/weather/Weather Overview/View Model/WeatherOverViewViewModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+class WeatherOverViewViewModel {
+    
+    var hasCities: Bool {return !AppManager.shared.userCitiesWeather.isEmpty}
+    var citiesWeather: [WeatherResponseModel] {return AppManager.shared.userCitiesWeather}
+    
+}
